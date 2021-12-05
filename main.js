bc2b3414e286089e6b055ec973b34f8fbc9109b6
Afhaam1 <84372124+Afhaam1@users.noreply.github.com>
2021-12-05 19:03:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    if(Content =="take my selfie")
+    {
+        speak()
+    }
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera)
+    setTimeout(function(){
+        take_snapshot()
+    save() },5000
+        )
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera")
+function take_snapshot(){
+
+    Webcam.snap(function(data_url)
+    {
+        document.getElementById("result").innerHTML = '<img id="image" src="'+data_url+'">';
+    })
+}
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("image").src ;
+    link.href=image;
+    link.click()
+}
